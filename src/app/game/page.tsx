1968673c0f6f3b792648df4267f7ac196c41384a
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-05-31 20:20:18 -0400

    break

---
+++ src/app/game/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Game() {
+    return (
+        <>
+        <div>
+            <h1>This is the game page</h1>
+            <Link href="/../">Go back to home page</Link>
+        </div>
+    
+        </>
+    )
+}
