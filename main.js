efa57939909c095219767e19a92e83f5a876b881
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-06-04 02:28:14 -0400

    good progress

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('main.js');
+let clicker = document.getElementById("clicker");
+let counter = document.getElementById("clicks");
+let clicks = 0;
+
+clicker.addEventListener("click", function() {
+    clicks++;
+    //update the counter
+    counter.innerHTML = 'coins:' + clicks;
+
+    }
+);
+
+
+//TODO create a function that will update a CPS realtime
